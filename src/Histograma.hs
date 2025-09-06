bf928e872bf7b43f089a20b27b689bde4f261e40
Paulo Ballan <pballan@dc.uba.ar>
2025-09-06 20:34:47 -0300

    first ocmmit

---
+++ src/Histograma.hs
@@ -0,0 +1,73 @@
+-- | Un `Histograma` es una estructura de datos que permite contar cuántos valores hay en cada rango.
+-- @vacio n (a, b)@ devuelve un histograma vacío con n+2 casilleros:
+--
+-- * @(-inf, a)@
+-- * @[a, a + tamIntervalo)@
+-- * @[a + tamIntervalo, a + 2*tamIntervalo)@
+-- * ...
+-- * @[b - tamIntervalo, b)@
+-- * @[b, +inf)@
+--
+-- `vacio`, `agregar` e `histograma` se usan para construir un histograma.
+module Histograma
+  ( Histograma, -- No se exportan los constructores
+    vacio,
+    agregar,
+    histograma,
+    Casillero (..),
+    casMinimo,
+    casMaximo,
+    casCantidad,
+    casPorcentaje,
+    casilleros,
+  )
+where
+
+import Data.List (zipWith4)
+import Util
+
+data Histograma = Histograma Float Float [Int]
+  deriving (Show, Eq)
+
+-- | Inicializa un histograma vacío con @n@ casilleros para representar
+-- valores en el rango y 2 casilleros adicionales para los valores fuera del rango.
+-- Require que @l < u@ y @n >= 1@.
+vacio :: Int -> (Float, Float) -> Histograma
+vacio n (l, u) = Histograma l ((u-l)/(fromIntegral n)) (replicate (n + 2) 0)
+--infinitoPositivo infinitoNegativo
+
+-- | Agrega un valor al histograma.
+
+--  Histograma z y [] --> z + (t*y) = x -> t
+agregar :: Float -> Histograma -> Histograma
+agregar x (Histograma z y xs) = Histograma z y (actualizarElem (floor ((x - z)/y) + 1) (+1) xs) 
+-- (-inf, 1.5), (1.5,2.0), (2.0, 2.5), (2.5,+inf)
+-- | Arma un histograma a partir de una lista de números reales con la cantidad de casilleros y rango indicados.
+histograma :: Int -> (Float, Float) -> [Float] -> Histograma
+histograma n r xs = foldr agregar (vacio n r) xs
+
+
+-- | Un `Casillero` representa un casillero del histograma con sus límites, cantidad y porcentaje.
+-- Invariante: Sea @Casillero m1 m2 c p@ entonces @m1 < m2@, @c >= 0@, @0 <= p <= 100@
+data Casillero = Casillero Float Float Int Float
+  deriving (Show, Eq)
+
+-- | Mínimo valor del casillero (el límite inferior puede ser @-inf@)
+casMinimo :: Casillero -> Float
+casMinimo (Casillero m _ _ _) = m
+
+-- | Máximo valor del casillero (el límite superior puede ser @+inf@)
+casMaximo :: Casillero -> Float
+casMaximo (Casillero _ m _ _) = m
+
+-- | Cantidad de valores en el casillero. Es un entero @>= 0@.
+casCantidad :: Casillero -> Int
+casCantidad (Casillero _ _ c _) = c
+
+-- | Porcentaje de valores en el casillero respecto al total de valores en el histograma. Va de 0 a 100.
+casPorcentaje :: Casillero -> Float
+casPorcentaje (Casillero _ _ _ p) = p
+
+-- | Dado un histograma, devuelve la lista de casilleros con sus límites, cantidad y porcentaje.
+casilleros :: Histograma -> [Casillero]
+casilleros (Histograma z y xs) = zipWith4 
---
+++ src/Util.hs
@@ -0,0 +1,24 @@
+module Util where
+
+-- | @alinearDerecha n s@ agrega espacios a la izquierda de @s@ hasta que su longitud sea @n@.
+-- Si @s@ ya tiene longitud @>= n@, devuelve @s@.
+alinearDerecha :: Int -> String -> String
+alinearDerecha n s
+    | length s >= n = s
+    | otherwise = replicate (n - length s) ' ' ++ s
+
+-- | Dado un índice y una función, actualiza el elemento en la posición del índice
+-- aplicando la función al valor actual. Si el índice está fuera de los límites
+-- de la lista, devuelve la lista sin cambios.
+-- El primer elemento de la lista es el índice 0.
+actualizarElem :: Int -> (a -> a) -> [a] -> [a]
+actualizarElem n f = zipWith (\i e -> if i == n then f e else e) [0..]
+
+-- | infinito positivo (Haskell no tiene literal para +infinito)
+infinitoPositivo :: Float
+infinitoPositivo = 1 / 0
+
+-- | infinito negativo (Haskell no tiene literal para -infinito)
+infinitoNegativo :: Float
+infinitoNegativo = -(1 / 0)
+
